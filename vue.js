268413174c582c81c451bc816eb7df2290157422
a16seplu <37794583+a16seplu@users.noreply.github.com>
2019-02-28 11:32:26 +0100

    Update vue.js

--- vue.js
+++ vue.js
@@ -1,5 +1,5 @@
 <!-- development version, includes helpful console warnings -->
-<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+//<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 main(){
 
 }
