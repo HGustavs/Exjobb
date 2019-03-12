7785584798a5086d126d27f4445c49e4fef6fa32
a16seplu <37794583+a16seplu@users.noreply.github.com>
2019-03-12 12:05:43 +0100

    Update javascript.js
    The current javascript is just very basic message parsing to make sure that i understand sort of how it works, this will change.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,52 @@
-function main(){
-alert("RASMUS ÄR BIG TREVLIG");
-}
+
+var header = new Vue({
+  el: '#header',
+  data:{
+    message:'Exjobs-Extra'
+  }
+});
+
+var menu = new Vue({
+  el: '#menu',
+  data:{
+    todos:[
+      { text: 'Inrikes'},
+      { text: 'Utrikes'},
+      { text: 'Sport'},
+      //{ text: 'för enkel utökning'}
+    ]
+  }
+});
+
+var news1 = new Vue ({
+  el: '#news1',
+  data:{
+    message:'Nyheter kommer vara här'
+  }
+});
+
+var news2 = new Vue ({
+  el: '#news2',
+  data:{
+    message:'Mer nyheter'
+  }
+});
+
+var news3 = new Vue ({
+  el: '#news3',
+  data:{
+    message:'Ännu mer nyheter'
+  }
+});
+
+var footer = new Vue({
+  el: '#footer',
+  data:{
+    todos:[
+      { text: 'Kontakt'},
+      { text: 'Examensarbete'},
+      { text: 'Terms of Service'},
+      //{ text: 'för enkel utökning'}
+    ]
+  }
+});
