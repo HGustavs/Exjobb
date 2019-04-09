3a2e693fa09dc20276dd592e1b5295acbb91a5f1
a16seplu <37794583+a16seplu@users.noreply.github.com>
2019-04-09 11:49:52 +0200

    Add files via upload

--- javascript-ang.js
+++ javascript-ang.js
@@ -1,6 +1,3 @@
-//<link rel="stylesheet" type="text/css" href="css-ang.css">
-//<script type="text/javascript" src="javascript-ang.js"></script>
-//<div><iframe width="560" height="315" src="https://www.youtube.com/embed/HgBN6eHzyBc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 
 angular.module('myApp', [])
 .controller('headCtrl', function($scope) {
@@ -13,26 +10,18 @@ angular.module('myApp', [])
     {category:'Utrikes'},
     {category:'Sport'}
   ];
-})
-.controller('menuCtrl', function($scope) {
-  $scope.categories = [
-    {category:'Inrikes'},
-    {category:'Utrikes'},
-    {category:'Sport'}
-  ];
-})
-.controller('contentOne', function($scope){
+
   $scope.articlesOne = [
     {headline:'Nyhet 1'},
     {text:'lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här '}
   ];
-})
-.controller('contentTwo', function($scope){
+
   $scope.articlesTwo = [
     {headline:'Nyhet 2'},
     {text:'lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här '}
   ];
 })
+
 .controller('contentThree', function($scope, $http){
   $scope.articleList = [];
   $scope.articleList.articleTitle = '';
@@ -46,6 +35,26 @@ angular.module('myApp', [])
   });
 })
 ;
+//.controller('menuCtrl', function($scope) {
+//  $scope.categories = [
+//    {category:'Inrikes'},
+//    {category:'Utrikes'},
+//    {category:'Sport'}
+//  ];
+//})
+//.controller('contentOne', function($scope){
+//  $scope.articlesOne = [
+//    {headline:'Nyhet 1'},
+//    {text:'lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem //ipsum här lorem ipsum här lorem ipsum här lorem ipsum här '}
+//  ];
+//})
+//.controller('contentTwo', function($scope){
+//  $scope.articlesTwo = [
+//    {headline:'Nyhet 2'},
+//    {text:'lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem //ipsum här lorem ipsum här lorem ipsum här lorem ipsum här '}
+//  ];
+//})
+
 //.controller('contentThree', function($scope){
 //  $scope.articlesThree = [
 //    {test:data}
