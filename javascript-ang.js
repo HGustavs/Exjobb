c3581ab8faac700b1040a217fc1da5dbbcfbe2e8
a16seplu <37794583+a16seplu@users.noreply.github.com>
2019-05-20 16:06:31 +0200

    Add files via upload
    final

--- javascript-ang.js
+++ javascript-ang.js
@@ -1,6 +1,6 @@
 
 angular.module('myApp', [])
-.controller('headCtrl', function($scope) {
+.controller('mainCtrl', function($scope) {
   $scope.header = [
     {text:'Examensarbete'}
   ];
@@ -33,41 +33,4 @@ angular.module('myApp', [])
   .then(function showArticles (response) {
       $scope.articleList = response.data;
   });
-})
-;
-//.controller('menuCtrl', function($scope) {
-//  $scope.categories = [
-//    {category:'Inrikes'},
-//    {category:'Utrikes'},
-//    {category:'Sport'}
-//  ];
-//})
-//.controller('contentOne', function($scope){
-//  $scope.articlesOne = [
-//    {headline:'Nyhet 1'},
-//    {text:'lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem //ipsum här lorem ipsum här lorem ipsum här lorem ipsum här '}
-//  ];
-//})
-//.controller('contentTwo', function($scope){
-//  $scope.articlesTwo = [
-//    {headline:'Nyhet 2'},
-//    {text:'lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem ipsum här lorem //ipsum här lorem ipsum här lorem ipsum här lorem ipsum här '}
-//  ];
-//})
-
-//.controller('contentThree', function($scope){
-//  $scope.articlesThree = [
-//    {test:data}
-//  ];
-//  return this.test
-//})
-
-//var articles = new Vue({
-//  el: '#data',
-//  data: data,
-//  computed:{
-//    showArticles: function(){
-//      return this.articles
-//    }
-//}
-//});
+});
